b5e16e972702eeb0e03a25de29ad0dbf16dc370c
rafijonnawada <108972502+rafijonnawada@users.noreply.github.com>
2022-07-12 14:49:26 +0530

    Create Day4_program1.c

---
+++ Day4/Day4_program1.c
@@ -0,0 +1,25 @@
+//ARITHMATICAL OPERATOR
+
+#include <stdio.h>
+
+int main()
+{
+    int x=30, y=20, z=10 , k;
+    k = x + y * z / 4 % 2 - 1 ;
+    printf("%d\n", k);
+    k= x% y+z*z;
+    printf("%d\n", k);
+    
+    k=  x*x-y/z;
+    printf("%d\n", k);
+    
+    k= x+y/z-y; 
+    printf("%d\n", k);
+    y=x+y-(x=y); //y=?
+    printf("%d\n", y);
+    //y=x+y-x=y ;   //y=?
+    //printf("%d", y);
+
+
+    return 0;
+}
